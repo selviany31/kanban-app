5eedf9f19653ae1bc9dc584e266a06c95a27936d
selviany31 <shelvy.any@gmail.com>
2023-01-09 21:32:19 +0800

    feat: adding dropdown on task component

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,5 +1,6 @@
-import { Card, Image, ProgressBar } from "react-bootstrap";
+import { Card, Dropdown, Image, ProgressBar } from "react-bootstrap";
 import "./Task.css"
+import SVG from "react-inlinesvg";
 
 const TaskComponent = () => {
     return ( 
@@ -12,7 +13,40 @@ const TaskComponent = () => {
                         style={{ height: "12px", width: "60%" }}
                     />
                     <p className="mb-0">60%</p>
-                    <Image src="icon/more.svg" />
+                    <Dropdown className="dropdown-custom">
+                        <Dropdown.Toggle
+                            className="bg-white border-0 p-0 m-0 d-flex justify-content-end"
+                        >
+                        <Image src="icon/more.svg" />
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu className="menu border-0">
+                            <Dropdown.Item className="d-flex align-items-center px-0 item-title">
+                                <div style={{ width: "35px" }}>
+                                    <SVG src="icon/arrow-right.svg"/>
+                                </div>
+                                <span>Move Right</span>
+                            </Dropdown.Item>
+                            <Dropdown.Item href="#/action-1" className="d-flex align-items-center px-0 item-title">
+                                <div style={{ width: "35px" }}>
+                                    <SVG src="icon/arrow-left.svg" />
+                                </div>
+                                <span className="item-title">Move Left</span>
+                            </Dropdown.Item>
+                            <Dropdown.Item href="#/action-1" className="d-flex align-items-center px-0 item-title">
+                                <div style={{ width: "35px" }}>
+                                    <SVG src="icon/edit.svg" />
+                                </div>
+                                <span className="item-title">Edit</span>
+                            </Dropdown.Item>
+                            <Dropdown.Item href="#/action-1" className="d-flex align-items-center px-0 item-title">
+                                <div style={{ width: "35px" }}>
+                                    <SVG  src="icon/trash.svg" />
+                                </div>
+                                <span className="item-title">Delete</span>
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </div>
             </Card.Body>
         </Card>
